2ffdb997cb542272a2f9a43a1c4769f3c780aefd
James Thompson <james.b.thompson@gmail.com>
2012-10-16 12:22:30 +0200

    increased page size

--- app/controllers/admin/portfolio_controller.rb
+++ app/controllers/admin/portfolio_controller.rb
@@ -1,7 +1,9 @@
 class Admin::PortfolioController < Admin::BaseController
 
   crudify :portfolio_entry,
+  		  :order => 'lft ASC',
           :conditions => {:parent_id => nil},
-          :sortable => true
+          :sortable => true,
+          :per_page => 200
 
 end
